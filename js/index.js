11dc0a866d618cfcba22cc3077f06296016df64c
Ciro Hesse <ciro.hesse@gmail.com>
2021-06-01 20:50:03 -0300

    gg

--- js/index.js
+++ js/index.js
@@ -6,11 +6,15 @@ function manejarClickBoton(){
     if (body.style.backgroundColor !== `black`){
         body.style.backgroundColor = `black`
         titulo.style.color = `white`
+        boton.style.color = `white`
+        boton.style.backgroundColor = `black`
         titulo.innerHTML = "Luz apagada"
         boton.innerHTML = "PRENDER"
     } else {
         body.style.backgroundColor = `white`
         titulo.style.color = `black`
+        boton.style.color = `black`
+        boton.style.backgroundColor = `white`
         titulo.innerHTML = "Luz prendida"
         boton.innerHTML = "APAGAR"
 
